52170fb2d5df0f427fa0ba426c33f97de4f64295
Frédéric Serva <fred.serva@gmail.com>
2015-02-05 17:55:25 +0100

    Initial commit

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Fired when the plugin is uninstalled.
+*
+* @package    Birds_Custom_Login
+* @since      1.0.0
+*/
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
